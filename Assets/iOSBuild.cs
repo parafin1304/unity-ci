2d9b9fea2ef901d3b87db7c5557c22d5a538c32d
author <pparafinski@gmail.com>
2018-05-11 10:05:51 +0200

    Projekt

---
+++ Assets/iOSBuild.cs
@@ -0,0 +1,20 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+public class BuildPlayerExample : MonoBehaviour
+{
+    public static void MyBuild()
+    {
+#if UNITY_EDITOR
+
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Scene1.unity" };
+        buildPlayerOptions.locationPathName = "iOSBuild";
+        buildPlayerOptions.target = BuildTarget.iOS;
+        buildPlayerOptions.options = BuildOptions.None;
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+#endif
+    }
+}
